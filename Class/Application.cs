e611c7358475cb99a611af89c4a57a3281c8b1fc
Bronid <bronidgames@gmail.com>
2022-12-17 13:54:11 +0100

    Changes in application class

--- Class/Application.cs
+++ Class/Application.cs
@@ -8,5 +8,32 @@ namespace ConsoleOOPShopCSharp.Class
 {
     internal class Application
     {
+        void Start()
+        {
+            Console.WriteLine("Welcome to ConsoleShopApplication!");
+            //There are we need add init functions
+        }
+
+        void printLine()
+        {
+            Console.WriteLine("-------------------------------------------");
+        }
+        void ShowMenu()
+        {
+            Console.WriteLine("Options: ");
+            printLine();
+            Console.WriteLine("1. Add Product");
+            printLine();
+            Console.WriteLine("2. Add Category");
+            printLine();
+            Console.WriteLine("3. Remove Product");
+            printLine();
+            Console.WriteLine("4. Remove Category");
+            printLine();
+            Console.WriteLine("5. Show Product");
+            printLine();
+            Console.WriteLine("6. Show Category");
+
+        }
     }
 }
